6731fd7d0859d1ae20f7861a5f421049a03333b8
Arvind Kang <42793089+Arvind-Kang@users.noreply.github.com>
2022-08-12 10:54:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const VNDB = require('vndb-api')
+
+const app = express();
+const https = require("https");
+
+const vndb = new VNDB('ArvindKang', {
+  // optionally, override any connection options you need to here, like
+  minConnection: 1,
+  maxConnection: 10,
+})
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', (req, res) => {
+  vndb
+    .query('get quote basic (id=18397)')
+    .then(response => {
+      // Use the response
+      console.log(response);
+      res.write(JSON.stringify(response));
+      res.end();
+    })
+    .catch(err => {
+      // Handle errors
+      console.log(err)
+    })
+
+});
+
+app.listen(process.env.PORT || 3000, () =>{
+  console.log("server is running on port 3000.");
+});
