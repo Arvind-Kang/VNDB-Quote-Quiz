efaecb18942d05eed54146ef05d1c262f0a81505
Arvind Kang <42793089+Arvind-Kang@users.noreply.github.com>
2022-08-15 03:52:02 +0100

    created app functionality

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const VNDB = require('vndb-api')
+const https = require("https");
 
 const app = express();
-const https = require("https");
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const vndb = new VNDB('ArvindKang', {
   // optionally, override any connection options you need to here, like
@@ -14,33 +19,64 @@ const vndb = new VNDB('ArvindKang', {
   maxConnection: 10,
 })
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  var day = "tuesday";
+  res.render('index');
 });
 
-app.post('/', (req, res) => {
-  var request = String(req.body.apiRequest);
-  console.log(request)
-  vndb
-    .query(request)
-    .then(response => {
-      // Use the response
-      console.log(response);
-      res.write(JSON.stringify(response));
-      res.end();
-    })
-    .catch(err => {
-      // Handle errors
-      console.log(err)
-      res.write(JSON.stringify(err));
-      res.end();
-    })
+app.post('/', async function(req, res){
+  var id = String(req.body.apiRequest);
+  let vnList = await call("get ulist basic, (uid=" + id +") {\"results\":100}")
+  var obj = (vnList.items)
+  var vnids = []
+  for (var vn of obj)
+  {
+    vnids.push(vn.vn)
+  }
+  var quoteList = [];
+
+  var more = true;
+  var page = 1
+  var x;
+  var quoteList = [];
+  while (more == true){
+    x = await call("get quote basic (id=[" + vnids.toString() +"]) {\"page\":"+ page +",\"results\":25,\"sort\":\"id\"}");
+    quoteList = quoteList.concat(x.items);
+    more = x.more
+    page+=1
+
+  }
+
+  var dict = {};
+  var nameDict = {}
+  for (var item of quoteList)
+  {
+    if (item.id in dict){
+      dict[item.id].push(item.quote);
+    } else{
+      dict[item.id] = [item.quote];
+      nameDict[item.id] = item.title
+    }
+  }
+  res.render('quiz', {dict: JSON.stringify(dict), nameDict: JSON.stringify(nameDict)});
 
 });
+
 
 app.listen(process.env.PORT || 3000, () =>{
   console.log("server is running on port 3000.");
 });
+
+async function call(query){
+  var res;
+  await vndb
+    .query(query)
+    .then(response => {
+      res = response;
+    })
+    .catch(err => {
+      // Handle errors
+      console.log(err)
+    })
+    return res
+}
